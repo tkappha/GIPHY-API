686f3bd258471436dd3a0f27cfa3cf9b68bbcb69
Tami <tkapphahn@gmail.com>
2017-05-23 16:11:30 -0700

    still has an API issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // ***** variables ********
 
 //array of animals
@@ -5,18 +7,31 @@ var animals = ["horse", "dog", "elephant", "cat", "monkey", "mouse","turtle", "f
 
 
 // creates buttons based on array of animals
+function populateButtons(arrayToUse, classToAdd, areaToAddTo){
+	$(areaToAddTo).empty();
 
-for (var i = 0; i < animals.length; i++){
-	console.log(i);
+	for (var i = 0; i < arrayToUse.length; i++){
+		var a = $("<button>");
+		a.addClass(classToAdd);
+		a.attr("data-type", arrayToUse[i]);
+		a.text(arrayToUse[i]);
+		$(areaToAddTo).append(a);
+
+	}
 
 }
 
 //event listener for all button elements
-$("button")on("click", function() {
-	var animal = $(this).attr("animals[i]-placeholder")
+$(document).on("click", ".animal-button", function() {
+	
+	$("#animals").empty();
+	$(".animal-button").removeClass("active");
+	$(this).addClass("active");
+
+	var animal = $(this).attr("data-type");
 
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-})
+
 
 $.ajax({
 	url: queryURL,
@@ -26,26 +41,33 @@ $.ajax({
 .done(function(response){
 		var results = response.data;
 
-	for(var i = 0; i < results.length; i++){
-		if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
-			var gifDiv = $("<div class = 'item'>");
+		for(var i = 0; i <results.length; i++){
+			var animalDiv = $("<div class=\"animal-item\">");
 			var rating = results[i].rating;
 			var p = $("<p>").text("Rating: " + rating);
+
+			var animated = results[i].images.fixed_height.url;
+			var still = results[i].images.fixed_height_still.url;
+
 			var animalImage = $("<img>");
 
-			animalImage.attr("src", results[i].images.fixed_height.url);
+			animalImage.attr("src", still);
+			animalImage.attr("data-still", still);
+			animalImage.attr("data-animate", animated);
+			animalImage.attr("data-state", "still");
+			animalImage.addClass("animal-image");
 
-			animals.append(p)
-			animals.append(animalImage);
+			animalDiv.append(p)
+			animalDiv.append(animalImage);
 
-			$("#animals").prepend(animals);
+			$("#animals").prepend(animalDiv);
 		}
-	}
+	});
 });
 
 // when gif is clicked, animates it if state was still, stops it if it's state was animate
-$(".gif").on("click", function(){
-	var state = $(this.attr("data-state");
+$(document).on("click",".animal-image", function(){
+	var state = $(this).attr("data-state");
 		if(state === "still"){
 			$(this).attr("src", $(this).attr("data-animate"));
 			$(this).attr("data-state", "animate");
@@ -53,5 +75,17 @@ $(".gif").on("click", function(){
 			$(this).attr("src", $(this).attr("data-still"));
 			$(this).attr("data-state", "still");
 		}
-		});
-})
+	});
+
+	$("#add-animal").on("click", function(event) {
+		event.preventDefault();
+		var newAnimal = $("input").eq(0).val();
+
+		if(newAnimal.length > 2) {
+			animals.push(newAnimal);
+		}
+		populateButtons(animals, "animal-button", "#animal-buttons");
+	});
+
+	populateButtons(animals, "animal-button", "#animal-buttons");
+});
